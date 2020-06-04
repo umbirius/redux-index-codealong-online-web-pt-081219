3186c1ccd8e8835f028295284089afd264fe5bec
umberto palazzo <upalazzo93@gmail.com>
2020-06-04 16:48:20 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -9,11 +9,15 @@ class CreateTodo extends Component {
       text: '',
     };
   }
-
+  
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    this.setState({
+      text: '',
+    })
   }
+
 
   handleChange(event) {
     this.setState({
@@ -22,15 +26,15 @@ class CreateTodo extends Component {
   }
 
   render() {
-    return(
+    return (
       <div>
         <form onSubmit={this.handleSubmit}>
-    	    <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+          <label>add todo</label>
+          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text} />
           <input type="submit" />
-       </form>
-     </div>
-   );
+        </form>
+      </div>
+    );
   }
 };
 
---
+++ src/components/todos/Todo.js
@@ -0,0 +1,11 @@
+// ./src/components/todos/Todo.js
+ 
+import React from 'react'
+ 
+const Todo = props => {
+  return (
+    <li>{props.text}</li>
+  );
+};
+ 
+export default Todo;
